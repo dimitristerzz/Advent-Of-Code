619aa79b95be7b8f4b52f1f2c469892fe7e4f43c
Dimitris Terzidis <dterzidis2008@gmail.com>
2025-12-03 23:39:16 +0200

    .

--- 2025/Day 2.py
+++ 2025/Day 2.py
@@ -17,17 +17,21 @@ for id in response.split(","):
     ranges.append((int(start), int(end)))
 
 ranges.sort()
-sum = 0
+invalid_ids = 0
 
 def check_validation(id: str):
     if len(id) % 2 == 0:
         mid = len(id) // 2
         if id[:mid] == id[mid:]:
             return True
+        else:
+            return False
+    else:
+        return False
 
 for start, end in ranges:
     for id in range(start, end + 1):
         if check_validation(str(id)) == True:
-            sum = sum + id
+            invalid_ids = invalid_ids + id
 
-print(sum)
+print(invalid_ids)
